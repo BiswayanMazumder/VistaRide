4dd49ff951c6a0848ced89aa2d0c4e7241562d9b
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-11-29 23:02:13 +0530

    Home Page creation #3

--- VistaRide Website/vistaride-website/src/Opening Page/LandingPage(Laptop).jsx
+++ VistaRide Website/vistaride-website/src/Opening Page/LandingPage(Laptop).jsx
@@ -27,15 +27,20 @@ export default function LandingPage_Laptop() {
             </div>
             <div className="nbnrnjrnf">
                 <div className="ehfefbefb">
-                <div className="jnjvn">
-                    Go anywhere with<br />VistaRide
-                </div>
-                <div className="jnjvn" style={{ marginTop: '30px' }}>
-                    <input type="text" className='ebfbebfeh' placeholder=' Pickup location'/>
-                </div>
-                <div className="jnjvn" style={{ marginTop: '20px' }}>
-                    <input type="text" className='ebfbebfeh' placeholder=' Dropoff location' />
-                </div>
+                    <div className="jnjvn">
+                        Go anywhere with<br />VistaRide
+                    </div>
+                    <div className="jnjvn" style={{ marginTop: '30px' }}>
+                        <input type="text" className='ebfbebfeh' placeholder=' Pickup location' />
+                    </div>
+                    <div className="jnjvn" style={{ marginTop: '20px' }}>
+                        <input type="text" className='ebfbebfeh' placeholder=' Dropoff location' />
+                    </div>
+                    <Link style={{ textDecoration: 'none', color: 'white' }}>
+                    <div className="jffnrn">
+                        See prices
+                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
