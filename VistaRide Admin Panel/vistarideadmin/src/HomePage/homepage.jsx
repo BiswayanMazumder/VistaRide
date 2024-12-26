ee205d3dcc0d550f5f94a5de8ea1bfe49189dd20
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-12-26 23:32:13 +0530

    Sidebar creation #1

--- VistaRide Admin Panel/vistarideadmin/src/HomePage/homepage.jsx
+++ VistaRide Admin Panel/vistarideadmin/src/HomePage/homepage.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
-  return (
-    <div className='webbody'>
-      
-    </div>
-  )
+    return (
+        <div className='webbody'>
+            <div className="jffnjvfnv">
+                <div className="mnvnfv">
+                    <div className="jjdnjvnv">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/vistafeedd.appspot.com/o/Assets%2FScreenshot_2024-11-22_204328-removebg-preview.png?alt=media&token=53712449-daaa-4f70-bb92-0ca64793111e" alt="" height={"80px"} width={"80px"} />
+                        <div className="enjfndj">
+                            <svg width="100" height="30" xmlns="http://www.w3.org/2000/svg">
+                                <rect width="100" height="30" fill="white" rx="5"></rect>
+                                <text x="50%" y="50%" font-family="'Lobster', cursive" font-size="21" fill="black" text-anchor="middle" alignment-baseline="middle" dominant-baseline="middle">VistaRide</text>
+                            </svg>
+                        </div>
+
+                    </div>
+                    <div className="nnnvnfnvf">
+                        HOME
+                    </div>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk">
+                            Dashboard
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Server Monitoring
+                        </div>
+                    </Link>
+                    <div className="nnnvnfnvf">
+                        MEMBERS
+                    </div>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk">
+                            Admin
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Rider
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Drivers
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Organization
+                        </div>
+                    </Link>
+                    <div className="nnnvnfnvf">
+                        SERVICES
+                    </div>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk">
+                            Admin
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Rider
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Drivers
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Organization
+                        </div>
+                    </Link>
+                    <div className="nnnvnfnvf">
+                        SERVICES
+                    </div>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk">
+                            Admin
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Rider
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Drivers
+                        </div>
+                    </Link>
+                    <Link style={{ textDecoration: 'none', color: "black" }} to="/dashboard">
+                        <div className="dnjnkfjk" style={{marginTop:'5px'}}>
+                            Organization
+                        </div>
+                    </Link>
+                </div>
+                <div className="dnjfnjn">
+
+                </div>
+            </div>
+        </div>
+    )
 }
